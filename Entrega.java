5970bbb1a3ca87f8e7c89ad8bd4155cb4909c029
ThaisANunes <80170583+ThaisANunes@users.noreply.github.com>
2021-04-05 18:40:34 -0300

    Entrega 05.04
    Entrega laços de repetição no java.

--- Entrega.java
+++ Entrega.java
@@ -1,63 +1,62 @@
-import java.util.Scanner;
-public class Entrega {
+package repeticao;
 
-	
+import java.util.Scanner;
+
+public class Entrega {
+		
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	
-	int n1,n2,n3,m; // Exercicio 1
-	
-	Scanner leia = new Scanner(System.in);
-	
-	System.out.println("Digite o primeiro numero inteiro: ");
-	n1 = leia.nextInt();
-	System.out.println("Digite o segundo numero inteiro: ");
-	n2 = leia.nextInt();
-	System.out.println("Digite o terceiro numero inteiro: ");
-	n3 = leia.nextInt();
-	
-	if (n1 > n2)
-	{
-		if(n1 > n3)
-			m = n1;
-		else
-			m = n3;				
-	}
-	else
-	{
-		if(n2 > n3)
-			m = n2;
-		else
-			m = n3;
-	}
-	
-	System.out.println("O maior numero é: "+m+"\n");
-	
-	
-	int id; //Exercicio 3
-	
-	Scanner leia1 = new Scanner(System.in);
-	
-	System.out.println("\nDigite a idade: ");
-	id = leia1.nextInt();
-	
-	if(id>=10  &&  id<=14)
-	{
-		System.out.println("Categoria infantil!");
-	}
-	else if(id>=15  &&  id<=17)
-	{
-		System.out.println("Categoria Juvenil!");
-	}
-	else if(id>=18  &&  id<=25)
-	{
-		System.out.println("Categoria Adulto!");
-	}
-	else
-	{
-		System.out.println("Idade fora do padrão...!");
-	}
 		
-	}
+		int x; //Exercício 1
+		
+		for(x=1000; x<=1999; x++)
+		{
+			if (x%11==5)
+			{
 				
+				System.out.println(x);
+			}
+		}
+		
+		
+		
+		int idade=0,total1=0,total2=0; //Exercício 3
+		
+		Scanner ler = new Scanner(System.in);
+		
+		while(idade!=-99)
+		{
+			System.out.println("\nDigite sua idade: ");
+			idade = ler.nextInt();
+			
+			if(idade<21) 
+			{
+				total1 = total1 + 1;
+			}
+			if(idade>50) 
+			{
+				total2 = total2 +1;
+			}
+			
+		}
+		System.out.println("Total com menos de 21 anos: "+total1+"\nTotal com mais de 50 anos: "+total2);
+	
+	
+		
+		int num,soma=0; //Exercício 5
+		
+		do
+		{	
+			System.out.println("\nDigite um numero do teclado: ");
+			num = ler.nextInt();
+			if(num>0);
+			{				
+				soma = soma + num;
+			}
+			
+		}
+		while (num!=0);
+		
+		System.out.println("Soma dos numeros: "+soma);
+	}
 }
